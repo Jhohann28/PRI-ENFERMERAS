9192246adc136e4f6540e5d4234c74ea57f282b8
farfanRogerBrandon <fvr0031807@est.univalle.edu>
2023-09-04 22:26:04 -0400

    Inicialización del proyecto y creación de clase conectora con firebase 040923

---
+++ Data/firebaseConfig.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBzqFMfz2JwI42xZIc2dur98dax_v5UoFc",
+  authDomain: "pri-enfermeras-daaae.firebaseapp.com",
+  projectId: "pri-enfermeras-daaae",
+  storageBucket: "pri-enfermeras-daaae.appspot.com",
+  messagingSenderId: "538616007752",
+  appId: "1:538616007752:web:cf3dd3d2e66dfb60f39c7d",
+  measurementId: "G-2N42SVWYEC"
+};
+
+// Initialize Firebase
+const appFirebase = initializeApp(firebaseConfig);
+const analytics = getAnalytics(appFirebase);
+
+export default appFirebase;
+
---
+++ Models/User.js
@@ -0,0 +1,14 @@
+
+class User{
+    constructor(personID, userAuthID, role, status, latitude, longitude, registrationDate, updateDate){
+        this.personID = personID;
+        this.userAuthID = userAuthID;
+        this.role = role;
+        this.status = status;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.registrationDate = registrationDate;
+        this.updateDate = updateDate;
+
+    }
+}
