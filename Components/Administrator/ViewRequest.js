531f0b0808645350683f59af6441d4f7947b4fdc
joelLopezz <ltj0029618@est.univalle.edu>
2023-10-08 06:50:39 -0400

    View Request Fucnt

--- Components/Administrator/ViewRequest.js
+++ Components/Administrator/ViewRequest.js
@@ -5,6 +5,41 @@ import soli from "../../assets/images/Windows/solicitud.png";
 
 const App = () => {
 
+  const [solicitud, setSolicitud] = useState(null);
+
+  useEffect(() => {
+    // DEBE RECIBIR EL ID DE LA SOLICITUD DE LA OTRA VENTANA POR EL BOTON AMPLIAR
+    const { solicitudId } = route.params;
+
+    const solicitudRef = doc(db, 'JobRequest', solicitudId);
+
+    getDoc(solicitudRef)
+      .then((docSnapshot) => {
+        if (docSnapshot.exists()) {
+          const solicitudData = docSnapshot.data();
+          setSolicitud(solicitudData);
+        } else {
+          console.log('La solicitud no existe');
+        }
+      })
+      .catch((error) => {
+        console.error('Error al obtener la solicitud:', error);
+      });
+  }, []);
+
+  const handleDescargarPDF = () => {
+    if (solicitud && solicitud.curriculumUrl) {
+      Linking.openURL(solicitud.curriculumUrl);
+    } else {
+      console.log('No se ha proporcionado una URL de curriculum válida');
+    }
+  };
+
+  if (!solicitud) {
+    return <Text>Cargando...</Text>;
+  }
+
+
   return (
 
     <View style={ViewRequestStyle.container}>
@@ -19,17 +54,18 @@ const App = () => {
 
       <View style={ViewRequestStyle.card}>
 
-      <Text style={ViewRequestStyle.name}>Ana </Text>
-      <Text style={ViewRequestStyle.especialidad}>Especialidad:</Text>
-      <Text style={ViewRequestStyle.telefono}>Nro. Telefono: </Text>
+      <Text style={ViewRequestStyle.name}> {solicitud.names}</Text>
+      <Text style={ViewRequestStyle.especialidad}>Especialidad:{solicitud.speciality}</Text>
+      <Text style={ViewRequestStyle.telefono}>Nro. Telefono: {solicitud.phone}</Text>
 
-      <Text style={ViewRequestStyle.experience}>Experiencia: </Text>
+      <Text style={ViewRequestStyle.experience}>Fecha de Titulacion: {solicitud.titulationDate}</Text>
+      <Text style={ViewRequestStyle.experience}>Instituto:{solicitud.graduationInstitution} </Text>
       <View style={ViewRequestStyle.pdf}>
 
           <Text style={ViewRequestStyle.curriculum}>Curriculum adjunto: 
           
           </Text>
-          <TouchableOpacity style={ViewRequestStyle.buttonCurric}>
+          <TouchableOpacity style={ViewRequestStyle.buttonCurric} onPress={handleDescargarPDF}>
                   <Text style={ViewRequestStyle.textButton}>Ver PDF curriculum</Text>
           </TouchableOpacity>
       </View>
